1eec4c2b9eb042785d88d50c2e7972a6b622b8b5
MaMnMu <thekingmmg14@gmail.com>
2021-03-28 20:34:33 +0200

    V3

--- programa.java
+++ programa.java
@@ -2,19 +2,40 @@ import java.util.Scanner;
 
 public class Programa
 {
+	public static String mostrar(double nums[])
+	{
+		String texto = "";
+		for (int i = 0; i < nums.length; i++)
+			texto = texto + nums[i] + ", ";
+		return texto;
+	}
+	
+	public static double suma(double nums[])
+	{
+		double suma = 0;
+		for (int i = 0; i < nums.length; i++)
+			suma = suma + nums[i];
+		return suma;
+	}
+	
+	public static double media(double suma, double nums[])
+	{
+		return suma / nums.length;
+	}
+	
 	public static void main(String [] args)
 	{
 		Scanner sc = new Scanner(System.in);
-		double suma = 0;
-		System.out.println("¿Cuantos numeros quieres sumar?");
+		System.out.println("Introduzca el tamaño de la lista");
 		int n = sc.nextInt();
-		for (int i = 0; i < n; i++)
+		double nums[] = new double[n];
+		for (int i = 0; i < nums.length; i++)
 		{
-			System.out.println("Introduzca un numero a sumar");
-			double nums = sc.nextDouble();
-			suma = suma + nums;
+			System.out.println("Introduzca un numero a la lista");
+			nums[i] = sc.nextDouble();
 		}
-		System.out.println("La suma de los numeros es " + suma);
-		System.out.println("La media de los numeros es " + suma / n);
+		System.out.println("Lista: " + mostrar(nums));
+		System.out.println("La suma de los numeros de la lista es " + suma(nums));
+		System.out.println("La media de los numeros de la lista es " + media(suma(nums), nums));
 	}
 }
