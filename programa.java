8bf3c11f14d1140c44003d06673069591545bde1
MaMnMu <thekingmmg14@gmail.com>
2021-03-28 20:07:51 +0200

    Nuevo cambio

--- programa.java
+++ programa.java
@@ -1,18 +1,20 @@
 import java.util.Scanner;
 
-public class Programav1 
+public class Programav1
 {
 	public static void main(String [] args)
 	{
 		Scanner sc = new Scanner(System.in);
-		int i = 0, suma = 0;
-		while (i < 5)
+		double suma = 0;
+		System.out.println("¿Cuantos numeros quieres sumar?");
+		int n = sc.nextInt();
+		for (int i = 0; i < n; i++)
 		{
-			System.out.println("Introduzca un numero a sumar (5 en total)");
-			int nums = sc.nextInt();
+			System.out.println("Introduzca un numero a sumar");
+			double nums = sc.nextDouble();
 			suma = suma + nums;
-			i++;
 		}
 		System.out.println("La suma de los numeros es " + suma);
+		System.out.println("La media de los numeros es " + suma / n);
 	}
 }
